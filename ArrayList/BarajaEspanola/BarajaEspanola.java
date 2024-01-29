8e159f8eb1754c234a75992809bcf330e64571af
Vinicio <calamot51@gmail.com>
2024-01-29 11:35:17 +0100

    10 cartas al azar y ordenadas en bastos, copas, espadas, oros

--- ArrayList/BarajaEspanola/BarajaEspanola.java
+++ ArrayList/BarajaEspanola/BarajaEspanola.java
@@ -1,19 +1,23 @@
+
 /*
  * Realiza un programa que escoja al azar 10 cartas de la baraja española (10 objetos
 de la clase Carta (numero, palo, nombre)). Emplea un objeto de la clase ArrayList
 para almacenarlas y asegúrate de que no se repite ninguna.
  */
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.Comparator;
 
 public class BarajaEspanola {
     public static void main(String[] args) {
         ArrayList<Carta> cartasSeleccionadas = new ArrayList<>();
         Set<Carta> cartasUnicas = new HashSet<>();
 
-        String[] palos = {"Oros", "Copas", "Espadas", "Bastos"};
+        String[] palos = { "Oros", "Copas", "Espadas", "Bastos" };
         Random random = new Random();
 
         while (cartasUnicas.size() < 10) {
@@ -27,10 +31,30 @@ public class BarajaEspanola {
             }
         }
 
-        // Imprimir las cartas seleccionadas
-        System.out.println("10 cartas seleccionadas al azar:");
+        // Ordenar las cartas seleccionadas
+        cartasSeleccionadas.sort(new CartaComparator());
+
+        // Imprimir las cartas ordenadas
+        System.out.println("10 cartas seleccionadas al azar y ordenadas:");
         for (Carta carta : cartasSeleccionadas) {
             System.out.println(carta);
         }
+
     }
+
+    public static class CartaComparator implements Comparator<Carta> {
+        private final static List<String> ordenPalos = Arrays.asList("Bastos", "Copas", "Espadas", "Oros");
+
+        @Override
+        public int compare(Carta c1, Carta c2) {
+            int paloCompare = Integer.compare(ordenPalos.indexOf(c1.getPalo()), ordenPalos.indexOf(c2.getPalo()));
+            if (paloCompare != 0) {
+                return paloCompare;
+            } else {
+                // Asumiendo que el As es 1, y Sota, Caballo, Rey son 10, 11, 12 respectivamente
+                return Integer.compare(c1.getNumero(), c2.getNumero());
+            }
+        }
+    }
+
 }
